f718b463e01b590ca4b14bc69c54d41bd0ce9c6d
Ferminioldy <pleukeulandryeli@gmail.com>
2025-05-04 20:32:45 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,17 +5,17 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KHAN-MD~kfFkFA6I#N8eje-tkpAcXC301Qug6aQrCgCKQIwqYTQr1t3ngOoE",
+SESSION_ID: process.env.SESSION_ID || "KHAN-MD~8W8G1IYS#QEfBYmG7EaSnLHwny2jjrhvyZsv9OMg9cJ9Vil16TR8",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
-// make true if you want auto reply on status 
+// make true if you want auto r
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY KHAN-MD 🤍*",
 // set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "true",
+WELCOME: process.env.WELCOME || "false",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group
@@ -23,9 +23,9 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/qnvl6h.jpeg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/60vdra.jpeg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || "(",
+PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "KHAN-MD",
 // add bot namw here for menu
@@ -37,13 +37,13 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "2250554432235",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "2250103482592",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "♨️𝕄ℝ 𝕂𝕀𝕐𝕆𝕋𝔸𝕂𝔸 ᴸᴰᵞ♨️",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/6p7h2x.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/60vdra.jpeg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *KHAN-MD*⚡",
 // add alive msg here 
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "true",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "2250554432235",
+DEV: process.env.DEV || "2250103482592",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
